76b0364f2f974cfa005090635d2ca31c50cb93a4
atharva2125 <atharvaprabhu17@gmail.com>
2025-02-09 00:10:35 +0530

    auth app 1

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+import Link from "next/link";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+export default function SignupPage() {
+    const router = useRouter();
+
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+        username: ""
+    });
+
+    const [buttonDisabled, setButtonDisabled] = React.useState(true);
+    const [loading, setLoading] = React.useState(false);
+
+    const onSignup = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/signup", user);
+            console.log("Sign up success", response.data);
+            router.push("/login");
+        } catch (error: any) {
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        if (user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+            setButtonDisabled(false);
+        } else {
+            setButtonDisabled(true);
+        }
+    }, [user]);
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>{loading ? "Signing up..." : "Sign up"}</h1>
+            <hr />
+            <label htmlFor="username">Username</label>
+            <input
+                className="p-2 border border-gray-300 text-black"
+                id="username"
+                type="text"
+                value={user.username}
+                onChange={(e) => setUser({ ...user, username: e.target.value })}
+                placeholder="Username"
+            />
+            <label htmlFor="email">Email</label>
+            <input
+                className="p-2 border border-gray-300 text-black"
+                id="email"
+                type="text"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                placeholder="Email"
+            />
+            <label htmlFor="password">Password</label>
+            <input
+                className="p-2 border border-gray-300 text-black"
+                id="password"
+                type="password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                placeholder="Password"
+            />
+            <button
+                className="p-2 bg-blue-500 text-white"
+                onClick={onSignup}
+                disabled={buttonDisabled}
+            >
+                {buttonDisabled ? "No Signup" : "Signup"}
+            </button>
+            <Link href="/login">Login</Link>
+        </div>
+    );
+}
